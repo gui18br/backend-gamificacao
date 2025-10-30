7e586638d57f4cef8e20ebad7e0266249a721317
Rafael Carvalho <rafacarvalho.10@outlook.com>
2025-10-30 10:20:07 -0300

    feat: adiciona schemas de criacao e resposta para Avatar

--- app/schemas/avatar.py
+++ app/schemas/avatar.py
@@ -1,12 +1,27 @@
+from typing import Optional, List
 from pydantic import BaseModel
 
 class AvatarBase(BaseModel):
-    id: int
     caminho_foto: str
-    nome: str
-        
+    nome: Optional[str] = None
+    
+class AvatarCreate(AvatarBase):
+    pass
+    
 class AvatarResponse(AvatarBase):
     id: int
+    
+    class Config:
+        from_attributes = True
+
+class AvatarResponseList(BaseModel):
+    data: List[AvatarResponse]
+    
+    class Config:
+        from_attributes = True
+
+class AvatarResponseSingle(BaseModel):
+    data: AvatarResponse
     
     class Config:
         from_attributes = True
